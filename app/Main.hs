fb7a6e664798c58f034b05d92cd63df0fdef58b2
Sergio Navarrete Suárez <eng.sergio.ns@gmail.com>
2025-02-07 12:41:18 -0500

    Connects with fen-to-image to send images of the board

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,6 @@ import           Data.Text                        (Text, pack, unpack)
 import Data.Maybe                                 (isJust, fromJust)
 import Control.Monad.IO.Class
 import Data.Text.Encoding
-import Debug.Trace                                (trace, traceM)
 
 import           Telegram.Bot.API
 import           Telegram.Bot.Simple
@@ -18,6 +17,8 @@ import Game.Chess.SAN
 import Database.Redis
 
 import Util
+import Board (fenToimage)
+import Data.Char (toLower)
 
 
 data Model = Model { redisConnection :: Connection }
@@ -38,7 +39,6 @@ getChatId update = case updateMessage update of
   Nothing -> ChatId 0
 
 updateToAction :: Update -> Model -> Maybe Action
-updateToAction update _ | trace ("updateToAction " ++ showMaybe (updateMessageText update)) False = undefined
 updateToAction update _ = case updateMessageText update of
       Just text -> do
         let moveText = getMove text
@@ -61,6 +61,7 @@ dbGet redisConn key = do
   case result of
     Right (Just value) -> return $ Just $ decodeLatin1 value
     Right _ -> return Nothing
+    -- TODO: Handle errors differently than just returning Nothing
     Left _ -> return Nothing
 
 dbSave :: MonadIO m => Connection -> Text -> Text -> m ()
@@ -70,6 +71,42 @@ dbSave redisConn key value = do
   _ <- liftIO $ runRedis redisConn $ set keyBB valueBB
   return ()
 
+dbDelete :: MonadIO m => Connection -> Text -> m ()
+dbDelete redisConn key = do
+  let keyBB = encodeUtf8 key
+  _ <- liftIO $ runRedis redisConn $ del [keyBB]
+  return ()
+
+sendImage :: Integer -> Text -> Maybe Ply -> BotM Text
+sendImage chatID fen maybePly = do
+  let sanMove = case maybePly of {
+    Just ply -> ((map toLower (show $ plySource ply)), (map toLower (show $ plyTarget ply))) ;
+    -- TODO: Change Board lib so that the ply is optional
+    Nothing -> ("", "")
+  }
+  let imageName = "board" ++ (show chatID) ++ ".png"
+  let inputFile = InputFile imageName "image/png"
+  _ <- liftIO $ fenToimage (unpack fen) sanMove 800 imageName
+  let photoFile = MakePhotoFile inputFile
+  let sendPhotoRequest = defSendPhoto (SomeChatId (ChatId chatID)) photoFile
+  response <- liftClientM $ sendPhoto sendPhotoRequest
+  if not $ responseOk response
+  then
+    pure "Can't return image"
+  else
+    pure fen
+
+performMove :: Position -> Text -> Integer -> Connection -> BotM Text
+performMove board sanMove chatID conn = case fromSAN board sanMove of
+  Right ply -> do
+    let chatIDT = pack $ show chatID
+    let newPosition = unsafeDoPly board ply
+    let newBoard = pack $ toFEN newPosition
+    let isGameFinished = length (legalPlies newPosition) == 0
+    _ <- if isGameFinished then dbDelete conn chatIDT else dbSave conn chatIDT newBoard
+    sendImage chatID newBoard (Just ply)
+  Left _ -> pure "Illegal move"
+
 handleAction :: Action -> Model -> Eff Action Model
 handleAction action model = case action of
   Answer msg -> model <# do
@@ -78,29 +115,14 @@ handleAction action model = case action of
     let conn = redisConnection model
     let chatIDT = pack $ show chatID
     boardDB <- dbGet conn chatIDT
-    _ <- traceM ("boardDB " ++ show boardDB)
     case boardDB of
       Just boardFEN -> do
         case fromFEN $ unpack boardFEN of
           Just board -> do
-            _ <- traceM ("board " ++ show board)
-            case fromSAN board moveText of
-              Right ply -> do
-                let newPosition = unsafeDoPly board ply
-                let newBoard = pack $ toFEN newPosition
-                _ <- dbSave conn chatIDT newBoard
-                pure $ newBoard
-              Left _ -> pure "Illegal move from board"
+            performMove board moveText chatID conn
           Nothing -> pure "Coulnd't parse board from FEN"
       Nothing -> do
-        _ <- traceM ("new board")
-        case fromSAN startpos moveText of
-          Right ply -> do
-            let newPosition = unsafeDoPly startpos ply
-            let newBoard = pack $ toFEN newPosition
-            _ <- dbSave conn chatIDT newBoard
-            pure newBoard
-          Left _ -> pure "Illegal initial move"
+        performMove startpos moveText chatID conn
 
 main :: IO ()
 main = do
