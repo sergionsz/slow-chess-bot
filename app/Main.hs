a0f2a3c4188bb2b67d7c7fdfa65ef3e3ec6c791f
Sergio Navarrete Suárez <eng.sergio.ns@gmail.com>
2025-02-07 12:41:18 -0500

    Implements game - Saves new moves to the DB

--- app/Main.hs
+++ app/Main.hs
@@ -1,48 +1,93 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import           Data.Text                        (Text)
-import           Data.Either                      (isRight)
+import           Data.Text                        (Text, pack, unpack)
+import Data.Maybe                                 (isJust, fromJust)
+import Control.Monad.IO.Class
+import Data.Text.Encoding
+import Debug.Trace                                (trace, traceM)
 
 import           Telegram.Bot.API
 import           Telegram.Bot.Simple
 import           Telegram.Bot.Simple.UpdateParser (updateMessageText)
-import Debug.Trace (trace)
+
+import Game.Chess
+import Game.Chess.SAN
+
+import Database.Redis
 
 import Util
 
-type Model = ()
 
-newtype Action = Echo Text
+data Model = Model { redisConnection :: Connection }
 
-slowChessBot :: BotApp Model Action
-slowChessBot = BotApp
-  { botInitialModel = ()
+data Action = AttemptMove Text ChatId | Answer Text
+
+slowChessBot :: Connection -> BotApp Model Action
+slowChessBot conn = BotApp
+  { botInitialModel = Model { redisConnection = conn }
   , botAction = updateToAction
   , botHandler = handleAction
   , botJobs = []
   }
 
+getChatId :: Update -> ChatId
+getChatId update = case updateMessage update of
+  Just message -> chatId $ messageChat message
+  Nothing -> ChatId 0
+
 updateToAction :: Update -> Model -> Maybe Action
-updateToAction update a | trace (show "updateToAction " ++ showMaybe (updateMessageText update) ++ " -- " ++ show a) False = undefined
+updateToAction update _ | trace ("updateToAction " ++ showMaybe (updateMessageText update)) False = undefined
 updateToAction update _ = case updateMessageText update of
       Just text -> do
-        let move = text2Move text
-        if isRight move
+        let moveText = getMove text
+        let chatID = getChatId update
+        if chatID == ChatId 0
+          then
+            Just (Answer "Can't find chatID")
+        else if isJust moveText
           then 
-          Just (Echo text)
-        else
-          Nothing
+          Just (AttemptMove (fromJust moveText) chatID)
+        else Nothing
       Nothing   -> Nothing
 
 handleAction :: Action -> Model -> Eff Action Model
 handleAction action model = case action of
-  Echo msg -> model <# do
-    trace (show "handleAction Echo " ++ show msg ++ " -- " ++ show model)
-    -- Filter messages and get only those that are a chess move
-      pure msg
+  Answer msg -> model <# do
+    pure msg
+  AttemptMove moveText chatID -> model <# do
+    let conn = redisConnection model
+    let chatIDBS = encodeUtf8 $ pack $ show $ chatID
+    boardDB <- liftIO $ runRedis conn $ get chatIDBS
+    _ <- traceM ("boardDB " ++ show boardDB)
+    case boardDB of
+      Right (Just boardFEN) -> do
+        case fromFEN $ unpack $ decodeLatin1 boardFEN of
+          Just board -> do
+            _ <- traceM ("board " ++ show board)
+            case fromSAN board moveText of
+              Right ply -> do
+                let newPosition = unsafeDoPly board ply
+                let newBoard = encodeUtf8 $ pack $ toFEN newPosition
+                _ <- liftIO $ runRedis conn $ set chatIDBS newBoard
+                pure $ decodeLatin1 newBoard
+              Left _ -> pure "Illegal move from board"
+          Nothing -> pure "Coulnd't parse board from FEN"
+      Right (Nothing) -> do
+        _ <- traceM ("new board")
+        case fromSAN startpos moveText of
+          Right ply -> do
+            let newPosition = unsafeDoPly startpos ply
+            let newBoard = encodeUtf8 $ pack $ toFEN newPosition
+            _ <- liftIO $ runRedis conn $ set chatIDBS newBoard
+            pure $ decodeLatin1 newBoard
+          Left _ -> pure "Illegal initial move"
+      Left _ -> pure "Something went wrong when trying to connect to the DB"
 
 main :: IO ()
 main = do
+  redis <- connect defaultConnectInfo
   token <- getEnvToken "TELEGRAM_BOT_TOKEN"
   env <- defaultTelegramClientEnv token
-  startBot_ slowChessBot env
+  startBot_ (slowChessBot redis) env
--- src/Util.hs
+++ src/Util.hs
@@ -1,11 +1,14 @@
-module Util (showMaybe, isMove, text2Move) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Util (
+  showMaybe,
+  getMove,
+) where
 
 import Data.Maybe (isJust)
 import           Data.Text                        (Text)
 import qualified Data.Text                        as Text
 import Data.Char as Char ( isAscii )
-import Game.Chess
-import Game.Chess.SAN
 
 showMaybe :: Show a => Maybe a -> [Char]
 showMaybe m
@@ -15,15 +18,12 @@ showMaybe m
 trim :: Text -> Text
 trim = Text.filter Char.isAscii
 
-isMove :: Text -> Bool
-isMove text 
-  | length (Text.words text) > 1 = False
-  | Text.length (trim text) > 8 = False
-  | otherwise = True
-
-text2Move :: Text -> Either String Ply
--- TODO: Add custom position
-text2Move text
-  | isMove text = fromSAN startpos text
-  | otherwise = Left "Wrong move"
+-- Use an indicator "->" to identify a chess move
+getMove :: Text -> Maybe Text
+getMove text = case Text.words (trim text) of
+  [] -> Nothing
+  (_:[]) -> Nothing
+  (indicator:moveText:_) -> 
+    if indicator /= "#" then Nothing else
+    if (Text.length moveText) > 8 then Nothing else Just moveText
   
